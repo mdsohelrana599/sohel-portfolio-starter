5931e5d687abb131a59a6d00edb15be96261e4ec
mdsohelrana599 <mdsohelrana812005@gmail.com>
2026-04-09 23:08:02 +0600

    TestimonialSlider

--- pages/about/index.js
+++ pages/about/index.js
@@ -144,7 +144,7 @@ const About = () => {
             <div className=" flex flex-1 xl:gap-x-6">
               <div className=" relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={1} duration={5}></CountUp> +
+                  <CountUp start={0} end={2} duration={5}></CountUp> +
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Years of experience
@@ -162,7 +162,7 @@ const About = () => {
 
               <div className=" relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={5} duration={5}></CountUp> +
+                  <CountUp start={0} end={10} duration={5}></CountUp> +
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Finished projects
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,34 @@
+import ParticlesContainer from "../../components/ParticlesContainer";
+import TestimonialSlider from "../../components/TestimonialSlider";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div>
+      <ParticlesContainer></ParticlesContainer>
+      <div className=" h-full bg-primary/30 py-32 text-center">
+        <div className=" container mx-auto h-full flex flex-col justify-center">
+          <motion.h2
+            variants={fadeIn("up", 0.2)} // Changed to "left" for better visual balance
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2 mb-8 xl:mb-0"
+          >
+            What clients <span className=" text-accent">say.</span>
+          </motion.h2>
+
+          <motion.div  variants={fadeIn("up", 0.4)} // Changed to "left" for better visual balance
+            initial="hidden"
+            animate="show"
+            exit="hidden">
+            <TestimonialSlider></TestimonialSlider>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
